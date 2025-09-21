5d9ad384d6e4cc28e32a4bc11a42ae5896b7f6df
Sooraj R Nair <soorajshagu@gmail.com>
2025-09-21 09:54:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,70 +1,68 @@
-//Initial References
 let movieNameRef = document.getElementById("movie-name");
 let searchBtn = document.getElementById("search-btn");
 let result = document.getElementById("result");
 
-//Function to fetch data from API
-let getMovie = () => {
-  let movieName = movieNameRef.value;
-  let url = `https://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
-  //If input field is empty
+// Search movies
+let getMovies = () => {
+  let movieName = movieNameRef.value.trim();
   if (movieName.length <= 0) {
-    result.innerHTML = `<h3 class="msg">Please Enter A Movie Name</h3>`;
+    result.innerHTML = `<h3 class="msg"></h3>`;
+    return;
   }
-  //If input field is NOT empty
-  else {
-    fetch(url)
-      .then((resp) => resp.json())
-      .then((data) => {
-        console.log(data);
-        console.log(data.Poster);
-        console.log(data.imdbRating);
-        console.log(data.Rated);
-        console.log(data.Year);
-        console.log(data.Runtime);
-        console.log(data.Genre);
-        console.log(data.Plot);
-        console.log(data.Actors);
-        console.log(data.length);
-        console.log(data.Title);
-        //If movie exists in database
-        if (data.Response == "True") {
-          result.innerHTML = `
-            <div class="info">
-                <img src=${data.Poster} class="poster">
-                <div>
-                    <h2>${data.Title}</h2>
-                    <div class="rating">
-                        <img src="star-icon.svg">
-                        <h4>${data.imdbRating}</h4>
-                    </div>
-                    <div class="details">
-                        <span>${data.Rated}</span>
-                        <span>${data.Year}</span>
-                        <span>${data.Runtime}</span>
-                    </div>
-                    <div class="genre">
-                        <div>${data.Genre.split(",").join("</div><div>")}</div>
-                    </div>
-                </div>
-            </div>
-            <h3>Plot:</h3>
-            <p>${data.Plot}</p>
-            <h3>Cast:</h3>
-            <p>${data.Actors}</p>
-            
-        `;
-        }
-        //If movie does NOT exists in database
-        else {
-          result.innerHTML = `<h3 class='msg'>${data.Error}</h3>`;
-        }
-      })
-      //If error occurs
-      .catch(() => {
-        result.innerHTML = `<h3 class="msg">Error Occured</h3>`;
-      });
-  }
+  let url = `https://www.omdbapi.com/?s=${movieName}&apikey=${key}`;
+
+  fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => {
+      if (data.Response == "True") {
+        result.innerHTML = data.Search.map(movie => `
+          <div class="movie-card" onclick="getMovieDetails('${movie.imdbID}')">
+            <img src="${movie.Poster !== "N/A" ? movie.Poster : "no-image.png"}">
+            <h3>${movie.Title}</h3>
+            <p>${movie.Year}</p>
+          </div>
+        `).join("");
+      } else {
+        result.innerHTML = `<h3 class="msg">${data.Error}</h3>`;
+      }
+    })
+    .catch(() => {
+      result.innerHTML = `<h3 class="msg">Error Occurred</h3>`;
+    });
 };
-searchBtn.addEventListener("click", getMovie);
-window.addEventListener("load", getMovie);
+
+function getMovieDetails(id) {
+  let url = `https://www.omdbapi.com/?i=${id}&apikey=${key}`;
+  fetch(url)
+    .then(resp => resp.json())
+    .then(movie => {
+      let modal = document.createElement("div");
+      modal.id = "movie-details";
+      modal.innerHTML = `
+        <div class="content">
+          <span style="float:right;cursor:pointer;" onclick="this.closest('#movie-details').remove()">❌</span>
+          <img src="${movie.Poster !== "N/A" ? movie.Poster : "no-image.png"}">
+          <div>
+            <h2>${movie.Title} (${movie.Year})</h2>
+            <p><b>Genre:</b> ${movie.Genre}</p>
+            <p><b>Director:</b> ${movie.Director}</p>
+            <p><b>Actors:</b> ${movie.Actors}</p>
+            <p><b>Plot:</b> ${movie.Plot}</p>
+            <p><b>IMDB Rating:</b> ⭐ ${movie.imdbRating}</p>
+          </div>
+        </div>
+      `;
+      document.body.appendChild(modal);
+      modal.style.display = "flex";
+    });
+}
+
+searchBtn.addEventListener("click", getMovies);
+
+movieNameRef.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    getMovies();
+  }
+});
+
+window.addEventListener("load", getMovies);
